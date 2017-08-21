d1584db016284e722e75a18f6c3f1e4f4c453f5a
brcourt <brandon@brandoncourtney.com>
2017-08-21 01:04:25 -0500

    tweaks

--- app/pictures/views.py
+++ app/pictures/views.py
@@ -25,18 +25,30 @@ def before_request():
 #                            new_message=new_message, new_post=new_post)
 
 @pictures.route('/photos')
+@pictures.route('/photos/<user>')
 @login_required
-def pictures():
+def pictures(user=None):
     os.chdir(app.root_path)
-    photo_array = glob.glob("static/images/users/" +
-                            g.user.handle + "/photos/*")
-    print('photos', photo_array)
-    print(app.root_path, os.getcwd())
+    if user is None:
+
+        photo_array = glob.glob("static/images/users/" +
+                                g.user.handle + "/photos/*")
+        print('photos', photo_array)
+        print(app.root_path, os.getcwd())
+        print("static/images/users/" + g.user.handle + "/photos/*")
+
+    else:
+        photo_array = glob.glob("static/images/users/" + user + "/photos/*")
+        print('photos', photo_array)
+        print(app.root_path, os.getcwd())
+        print("static/images/users/" + user + "/photos/*")
 
     user_count = len(User.query.all())
-    new_message = len(Message.query.filter(Message.recipient == g.user.handle)
-                      .filter(Message.read is False).all()) or ""
-    new_post = len(Post.query.filter(Post.id > g.user.last_post).all()) or ""
+    new_message = len(Message.query.filter
+                      (Message.recipient == g.user.handle).filter
+                      (Message.read is False).all()) or ""
+    new_post = len(Post.query.filter(Post.id > g.user.last_post)
+                   .all()) or ""
 
     return render_template('pictures.html', user_count=user_count,
                            new_message=new_message, new_post=new_post,
